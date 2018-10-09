02ac10dd1843b476f210ac7acef43cb0d3ec73bd
mjpalanker <mjpalanker@gmail.com>
2018-10-09 10:48:15 -0700

    check_sentence

--- SpellCheck.py
+++ SpellCheck.py
@@ -22,6 +22,9 @@ class SpellChecker(object):
         score = lambda x, y: self.language_model.bigram_prob(x,y)
         return (score(prev_word, focus_word) + score(focus_word, next_word))/(2.0)
 
+    def unigram_score(self, word):
+        return self.language_model.unigram_prob(w)
+
     def inserts(self, word):
         l = []
         for i in range(len(word)):
@@ -42,9 +45,6 @@ class SpellChecker(object):
                 l.append(word[:i] + char + word[i+1:])
         return [x for x in l if x in self.language_model]
 
-    def unigram_score(self, word):
-        return self.language_model.unigram_prob(w)
-
     def cm_score(self, error_word, corrected_word):
         return self.channel_model.align(error_word, corrected_word)[0]
 
@@ -53,8 +53,36 @@ class SpellChecker(object):
         for i in range(self.max_distance):
             nested = list(map(self._one_step, source))
             flat = [l for sublist in nested for l in sublist]
-            source = flat
-        return flat
+            source = list(set(flat))
+        return source
+
+    def check_sentence(sentence, fallback=False):
+        l = []
+        for i in range(len(sentence)):
+            word = sentence[i]
+            if word in self.language_model:
+                l.append([word])
+            else:
+                choices = self.generate_candidates(word)
+                if len(choices) == 0:
+                    if fallback:
+                        l.append([word])
+                if i<1:
+                    prev_word = '%'
+                else:
+                    prev_word = sentence[i-1]
+
+                if i+1 == len(sentence):
+                    next_word = '%'
+                else:
+                    next_word = sentence[i+1]
+                rank = lambda x: self.cm_score(x, word) + self.bigram_score(prev_word, x, next_word)
+                ranked = sorted(choices, key = rank, reverse=True)
+                l.append(list(ranked))
+
+
+        return l
+
 
     def _one_step(self, word):
         return self.inserts(word) + self.deletes(word) + self.substitutions(word)
